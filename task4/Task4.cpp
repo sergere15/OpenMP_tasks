fd840382bd74333b3fe53f295ace04afb3e0985a
sergere15 <43388544+sergere15@users.noreply.github.com>
2020-01-10 16:06:16 +0300

    Fixed some bugs

--- task4/Task4.cpp
+++ task4/Task4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <omp.h>
+#include <ctime>
 
 void mult_martices(int** matrix1, int** matrix2, int** res_matrix, int M, int N, int K, int parall) {
 
@@ -17,50 +18,54 @@ void mult_martices(int** matrix1, int** matrix2, int** res_matrix, int M, int N,
 			}
 		}
 		end = omp_get_wtime();
-		std::cout << "Thread used: " << p << ",  time on common:" << (end - start) << std::endl;
+		std::cout << "Without threads,  time on common:" << (end - start) << std::endl;
 	}
 
 	if (parall == 1) {
-		start = omp_get_wtime();
-		for (int i = 0; i < N; i++) {
-#pragma omp parallel for
-			for (int j = 0; j < K; j++) {
-				for (int t = 0; t < M; t++)
-				{
-					res_matrix[i][j] += matrix1[i][t] * matrix2[t][j];
-				}
-			}
-		}
-		end = omp_get_wtime();
-		std::cout << "Thread used: " << p << ",  time on lenta1:" << (end - start) << std::endl;
-	}
-
-	if (parall == 2) {
-		for (int i = 0; i < N; i++) {
-#pragma omp parallel for
-			for (int j = 0; j < K; j++) {
-				for (int t = 0; t < M; t++)
-				{
-					res_matrix[i][j] += matrix1[i][t] * matrix2[t][j];
-				}
-			}
-		}
-	}
-
-	if (parall == 3) {
-		//by blocks 
 		for (int p = 1; p < 20; p++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < K; j++)
+					res_matrix[i][j] = 0;
+			}
+
 			omp_set_num_threads(p);
 			start = omp_get_wtime();
+			for (int i = 0; i < N; i++) {
+#pragma omp parallel for
+				for (int j = 0; j < K; j++) {
+					for (int t = 0; t < M; t++)
+					{
+						res_matrix[i][j] += matrix1[i][t] * matrix2[t][j];
+					}
+				}
+			}
+			end = omp_get_wtime();
+			std::cout << "Thread used: " << p << ",  time on tape:" << (end - start) << std::endl;
+		}
+	}
 
-#pragma omp parallel num_threads(threads_num)
+	
+
+	if (parall == 2) {
+		//by blocks 
+		for (int p = 1; p < 20; p++) {
+
+			omp_set_num_threads(p);
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < K; j++)
+					res_matrix[i][j] = 0;
+			}
+
+			start = omp_get_wtime();
+
+#pragma omp parallel
 			{
 				int p = omp_get_num_threads();
 				int q = p;
 				int r = p;
-				int a_block_height = M / p;
-				int a_block_width = N / q;
-				int b_block_heigth = N / q;
+				int a_block_height = N / p;
+				int a_block_width = M / q;
+				int b_block_heigth = M / q;
 				int b_block_width = K / r;
 #pragma omp for
 				for (int a_block_count = 0; a_block_count < p * q; a_block_count++)
@@ -85,7 +90,57 @@ void mult_martices(int** matrix1, int** matrix2, int** res_matrix, int M, int N,
 }
 
 int main() {
+	int N;
+	int M;
+	int K;
 
+
+	for (int p = 1; p <= 5; p++) {
+		//std::cout << "Input N, M:  " << std::endl;
+		//std::cin >> N >> M;
+		std::cout << std::endl;
+		N = 100 * p;
+		M = 150 * p;
+		K = 120 * p;
+		double start_gen = clock();
+
+		int** matrix1 = new int*[N];
+		for (int i = 0; i < N; i++) {
+			matrix1[i] = new int[M];
+			for (int j = 0; j < M; j++) {
+				matrix1[i][j] = rand() % 1000;
+			}
+		}
+
+		int** matrix2 = new int*[M];
+		for (int i = 0; i < M; i++) {
+			matrix2[i] = new int[K];
+			for (int j = 0; j < K; j++) {
+				matrix2[i][j] = rand() % 1000;
+			}
+		}
+
+		int** res = new int*[N];
+		for (int i = 0; i < N; i++) {
+			res[i] = new int[K];
+			for (int j = 0; j < K; j++)
+
+			res[i][j] = 0;
+		}
+
+		double end_gen = clock();
+
+		std::cout << std::endl << "N= " << N << " M= " << M << " K= " << K << std::endl;
+		std::cout << "Generation time: " << (end_gen - start_gen) / CLK_TCK << std::endl;
+
+		mult_martices(matrix1, matrix2, res, M, N, K, 0);//Common
+		std::cout << std::endl;
+		mult_martices(matrix1, matrix2, res, M, N, K, 1);//By Tape 
+		std::cout << std::endl;
+
+		mult_martices(matrix1, matrix2, res, M, N, K, 3);//By blocks 
+		
+	}
 
 	system("pause");
 	return 0;
